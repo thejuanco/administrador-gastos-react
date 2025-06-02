282ff54efc7a5ed20eb44f8d4cb63f5077592768
Juanco <juanffo874@gmail.com>
2025-06-01 23:59:12 -0600

    Muestra los datos de los gastos en la vista

--- src/components/SpendingHistory.jsx
+++ src/components/SpendingHistory.jsx
@@ -2,12 +2,35 @@ import { useAppContext } from "../context/AppContext"
 
 const SpendingHistory = () => {
     const {spends} = useAppContext()
-    console.log(spends)
-    
+    console.log(spends.length)
+
   return (
     <>
       <div className="border border-gray-300 rounded-md h-96 mt-4 p-4">
-        <h1 className="font-semibold text-xl">Presupuesto General</h1>
+        <h1 className="font-semibold text-xl">Historial de Gastos</h1>
+        <form>
+            <input
+                type="text"
+                placeholder="Buscar gastos..."
+                className="w-full p-2 border border-gray-300 rounded-md my-2"
+            />
+        </form>
+
+        {spends.length > 0 ? (
+            <div>
+                {spends.map((spend, index) => (
+                    <div key={index} className="py-2 mx-2">
+                        <h2 className="font-semibold">{spend.description}</h2>
+                        <div className="flex justify-between">
+                            <p className="text-gray-400 text-sm">{spend.createdAt}</p>
+                            <p className="font-semibold text-gray-700 text-lg">$ {spend.amount}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        ) : (
+            <p className="text-center font-semibold text-lg pt-8">Aun no hay gastos</p>
+        )}
       </div>
     </>
   )
