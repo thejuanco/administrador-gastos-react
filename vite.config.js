eab64e24ed0b4920afe2eef3a4f91f54d9942352
Juanco <juanffo874@gmail.com>
2025-06-12 19:26:56 -0600

    Agrega una fuente para usos futuros

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  }
 })
