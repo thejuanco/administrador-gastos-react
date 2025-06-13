372a945dc2f2c9a3b6127a40c19834041c6ed119
Juanco <juanffo874@gmail.com>
2025-06-12 19:26:36 -0600

    Agrega un icono al boton de agregar gasto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,10 +17,13 @@ const Home = () => {
         <div className="flex justify-between py-6 px-10">
             <h1 className="font-semibold text-2xl">Administrador de Gastos</h1>
             <button
-                className="bg-gray-900 text-gray-200 px-5 py-2 rounded-lg hover:bg-slate-800"
+                className="bg-gray-800 text-gray-200 px-2 py-2 rounded-full font-semibold pr-6 hover:bg-slate-900 flex hover:cursor-pointer"
                 onClick={openModal}
             >
-                Agregar gasto
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mx-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                Agregar Gasto
             </button>
              
             <ModalSpend visible={openModal} onClose={closeModal} isOpen={isOpen} setIsOpen={setIsOpen}/>
