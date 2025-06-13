53482960de1d18c02af511b57a483909965f4a72
Juanco <juanffo874@gmail.com>
2025-06-12 19:55:56 -0600

    Agrega un button icon para eliminar el presupuesto

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -26,12 +26,19 @@ const Budget = () => {
   return (
     <>
       <div className="p-4 border border-gray-300 rounded-lg">
-        <h1 className="font-semibold text-xl">Presupuesto General</h1>
 
         {budget.length > 0 ? (
           <div>
             {budget.map((budget, index) => (
                 <div key={index}>
+                  <div className="flex justify-between">
+                    <h1 className="font-semibold text-xl">Presupuesto General</h1>
+                    <button className="hover:bg-gray-100 rounded-lg">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                      </svg>
+                    </button>
+                  </div>
                     <p className="text-sm text-gray-500">{budget.budget_description}</p>
                     <div className="grid grid-cols-2">
                         <div className="bg-blue-100 p-1 m-2 rounded-lg">
@@ -66,6 +73,7 @@ const Budget = () => {
           </div>
         ) : (
           <div className="mt-6">
+            <h1 className="font-semibold text-xl">Presupuesto General</h1>
             <form
               onSubmit={handleSubmit(onSubmit)}
               className="flex flex-col space-y-3"
