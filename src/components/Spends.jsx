b0b081d12aea5781ed442b49fe4d7db93b5869ed
Juanco <juanffo874@gmail.com>
2025-06-21 13:17:09 -0600

    Mostrando la suma de los gastos en el componente spends

--- src/components/Spends.jsx
+++ src/components/Spends.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useAppContext } from '../context/AppContext'
 
 const Spends = () => {
+  const {spends, setSpends} = useAppContext()
+  console.log(spends)
+
+  const result = spends.reduce((accumulator, currentValue) =>{
+    const totalSpends = currentValue.amount
+    return accumulator + totalSpends
+  }, 0)
+
   return (
     <>
         <div className="p-4 border border-gray-300 rounded-lg">
@@ -9,9 +18,14 @@ const Spends = () => {
             <div className="mt-6">
               <div className="flex justify-between">
                 <p className="text-lg font-semibold">Total</p>
-                <span className="text-xl font-bold">$2985.00</span>
+                <span className="text-xl font-bold">$ {result}</span>
               </div>
               <div className="h-px bg-gray-400 my-2"></div>
+              {spends.map((item, index) => (
+                <div key={index} className="mt-1 flex justify-end px-2">
+                  <p>$ {item.amount}</p>
+                </div>
+              ))}
             </div>
         </div>
     </>
