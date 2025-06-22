87aba13a4d91aed6b93053aae67618a3b55d4008
Juanco <juanffo874@gmail.com>
2025-06-21 22:36:22 -0600

    Valida si el objeto de spends contiene algo sino muestra un mensaje

--- src/components/Spends.jsx
+++ src/components/Spends.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAppContext } from '../context/AppContext'
 
 const Spends = () => {
-  const {spends, setSpends} = useAppContext()
+  const {spends, setSpends, budget} = useAppContext()
 
   const result = spends.reduce((accumulator, currentValue) =>{
     const totalSpends = currentValue.amount
@@ -14,18 +14,24 @@ const Spends = () => {
         <div className="p-4 border border-gray-300 rounded-lg">
             <h1 className="font-semibold text-xl">Resumen de Gastos</h1>
 
-            <div className="mt-6">
-              <div className="flex justify-between">
-                <p className="text-lg font-semibold">Total</p>
-                <span className="text-xl font-bold">$ {result}</span>
+            {Object.keys(budget).length === 0 ? (
+              <div className="mt-10 text-center">
+                <h1 className="text-gray-500">No hay gastos aún</h1>
               </div>
-              <div className="h-px bg-gray-400 my-2"></div>
-              {spends.map((item, index) => (
-                <div key={index} className="mt-1 flex justify-end px-2">
-                  <p>$ {item.amount}</p>
+            ) : (
+              <div className="mt-6">
+                <div className="flex justify-between">
+                  <p className="text-lg font-semibold">Total</p>
+                  <span className="text-xl font-bold">$ {result}</span>
                 </div>
-              ))}
+                <div className="h-px bg-gray-400 my-2"></div>
+                {spends.map((item, index) => (
+                  <div key={index} className="mt-1 flex justify-end px-2">
+                    <p>$ {item.amount}</p>
+                  </div>
+                ))}
             </div>
+            )}
         </div>
     </>
   )
