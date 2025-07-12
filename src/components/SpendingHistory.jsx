bb074511c366bac0208de78db9e2b9c27fbf4427
Juanco <juanffo874@gmail.com>
2025-07-12 16:22:07 -0600

    Realiza una busqueda por gastos

--- src/components/SpendingHistory.jsx
+++ src/components/SpendingHistory.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import { useAppContext } from "../context/AppContext"
 
 const SpendingHistory = () => {
+    const [search, setSearch] = useState("")
     const {spends} = useAppContext()
-    // console.log(spends.length)
 
     //Formatear fechas
     function formattedData(date) {
@@ -15,6 +16,11 @@ const SpendingHistory = () => {
       });
     }
 
+    //Devuelve los gastos filtrados
+    const filteredSpends = spends.filter(spend => {
+      return spend.description.toLowerCase().includes(search.toLowerCase())
+    })
+
   return (
     <>
       <div className="border border-gray-300 rounded-md h-min mt-4 p-4">
@@ -24,23 +30,31 @@ const SpendingHistory = () => {
                 type="text"
                 placeholder="Buscar gastos..."
                 className="w-full p-2 border border-gray-300 rounded-md my-2"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
             />
         </form>
 
-        {spends.length > 0 ? (
-            <div>
-                {spends.map((spend, index) => (
-                    <div key={index} className="py-2 mx-2">
-                        <h2 className="font-semibold">{spend.description}</h2>
-                        <div className="flex justify-between">
-                            <p className="text-gray-400 text-sm">{formattedData(spend.createdAt)}</p>
-                            <p className="font-semibold text-gray-700 text-lg">$ {spend.amount}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+        {filteredSpends.length > 0 ? (
+          <div>
+            {filteredSpends.map((spend, index) => (
+              <div key={index} className="py-2 mx-2">
+                <h2 className="font-semibold">{spend.description}</h2>
+                <div className="flex justify-between">
+                  <p className="text-gray-400 text-sm">
+                    {formattedData(spend.createdAt)}
+                  </p>
+                  <p className="font-semibold text-gray-700 text-lg">
+                    $ {spend.amount}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         ) : (
-            <p className="text-center font-semibold text-lg pt-8">Aun no hay gastos</p>
+          <p className="text-center font-semibold text-lg pt-8">
+            No se encontraron gastos
+          </p>
         )}
       </div>
     </>
